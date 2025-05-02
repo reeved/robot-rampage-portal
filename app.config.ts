dbf4e6755835a558f9650d0c585605a63caba7c1
Reeve D'Cunha <reeve@joyoushq.com>
2025-05-02 17:15:23 +1200

    init

---
+++ app.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "@tanstack/react-start/config";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+
+import { env } from "./src/env";
+
+const config = defineConfig({
+  tsr: {
+    appDirectory: "src",
+  },
+  vite: {
+    plugins: [
+      // this is the plugin that enables path aliases
+      viteTsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+      tailwindcss(),
+    ],
+  },
+});
+
+export default config;
