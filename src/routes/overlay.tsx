81b5d2ee819081601e8a9d882abdbdc5306124c5
jacksynergine <jack.barker@synergine.com>
2025-05-12 13:23:19 +1200

    Fixed overlay transparency

--- src/routes/overlay.tsx
+++ src/routes/overlay.tsx
@@ -6,24 +6,27 @@ export const Route = createFileRoute("/overlay")({
 
 function RouteComponent() {
 	return (
-		<div className="h-full w-full bg-white">
-			<div className="h-[1080px] w-[1920px] bg-transparent relative box-border">
-				<div className="absolute bottom-10 left-60 right-60 flex">
-					<div className="h-24 flex flex-1 bg-black/70 rounded-l-3xl items-center justify-center font-rubik text-4xl relative">
-						BAD BLOOD
-						<div className="absolute bottom-24 bg-amber-400/70 px-8 py-2 rounded-t-xl">
-							Win by KO
+		<>
+			<style>{`html, body { background: transparent !important; }`}</style>
+			<div className="h-full w-full">
+				<div className="h-[1080px] w-[1920px] relative box-border">
+					<div className="absolute bottom-10 left-60 right-60 flex">
+						<div className="h-24 flex flex-1 bg-black/70 rounded-l-3xl items-center justify-center font-rubik text-4xl relative">
+							BAD BLOOD
+							<div className="absolute bottom-24 bg-amber-400/70 px-8 py-2 rounded-t-xl">
+								Win by KO
+							</div>
 						</div>
-					</div>
-					<div className="h-24 w-[25ch] bg-primary/70" />
-					<div className="h-24 flex flex-1 bg-black/70 rounded-r-3xl items-center justify-center font-rubik text-4xl relative">
-						SNOLLYGOSTER
-						<div className="absolute bottom-24 bg-amber-400/70 px-8 py-2 rounded-t-xl">
-							Win by KO
+						<div className="h-24 w-[25ch] bg-primary/70" />
+						<div className="h-24 flex flex-1 bg-black/70 rounded-r-3xl items-center justify-center font-rubik text-4xl relative">
+							SNOLLYGOSTER
+							<div className="absolute bottom-24 bg-amber-400/70 px-8 py-2 rounded-t-xl">
+								Win by KO
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
