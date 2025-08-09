709a3771cad1252e3a29b50d709c72f075d0cd51
Reeve D'Cunha <dcunhareeve@gmail.com>
2025-08-10 02:52:30 +1200

    update teams match layout

--- src/routes/_view/teams-match/$id.tsx
+++ src/routes/_view/teams-match/$id.tsx
@@ -53,32 +53,48 @@ const BotInfo = ({
 	const botDetails = participants.find((p) => p.id === bot?.id);
 
 	return (
-		<div className={cn("flex flex-col flex-1 gap-y-4", teamColor === "yellow" ? "text-rryellow" : "text-rrblue")}>
-			{bot ? (
-				<img
-					src={botDetails?.photo ? `/${botDetails.photo}` : undefined}
-					className={cn(
-						"w-8/12 -mt-10 mx-auto rounded-3xl",
-						teamColor === "blue" && "transform -scale-x-100",
-						bot?.isDead ? "grayscale greyscale-manual" : "animate-breathing",
-					)}
-					alt="bot-photo"
-					height={160}
-					style={
-						bot?.isDead
-							? {}
-							: ({
-									filter: "drop-shadow(0 0 35px currentColor)",
-									// This custom property will be used by the animation
-									"--shadow-color": "currentColor",
-								} as React.CSSProperties)
-					}
-				/>
-			) : (
-				<div className="h-[160px]" />
+		<div
+			className={cn(
+				"flex flex-col flex-1 gap-y-4 min-h-[400px]",
+				teamColor === "yellow" ? "text-rryellow" : "text-rrblue",
 			)}
-			<div className="text-6xl font-rubik -mt-20 z-20">{botDetails?.name ?? ""} </div>
-			<div className="text-2xl font-heading uppercase text-white mt-4">{botDetails?.weapon ?? ""}</div>
+		>
+			{bot ? (
+				<div className="flex items-center justify-center flex-1 min-h-[300px]">
+					<img
+						src={botDetails?.photo ? `/${botDetails.photo}` : undefined}
+						className={cn(
+							"max-w-9/12 max-h-100 object-contain -mt-10 mx-auto rounded-3xl",
+							teamColor === "blue" && "transform -scale-x-100",
+							bot?.isDead ? "grayscale greyscale-manual" : "animate-breathing",
+						)}
+						alt="bot-photo"
+						height={400}
+						style={
+							bot?.isDead
+								? {}
+								: ({
+										filter: "drop-shadow(0 0 35px currentColor)",
+										// This custom property will be used by the animation
+										"--shadow-color": "currentColor",
+									} as React.CSSProperties)
+						}
+					/>
+				</div>
+			) : (
+				<div className="flex-1 min-h-[300px] flex items-center justify-center" />
+			)}
+			<div
+				className={cn(
+					"text-6xl font-rubik z-20 min-h-[60px] flex items-center justify-center",
+					botDetails?.name?.length && botDetails?.name?.length > 20 && "text-4xl",
+				)}
+			>
+				{botDetails?.name ?? ""}{" "}
+			</div>
+			<div className="text-2xl font-heading uppercase text-white mt-4 min-h-[32px] flex items-center justify-center">
+				{botDetails?.weapon ?? ""}
+			</div>
 		</div>
 	);
 };
@@ -96,9 +112,9 @@ const BotPreview = ({
 
 	if (!botDetails) {
 		return (
-			<div className="h-40 flex items-center justify-center">
+			<div className="h-20 flex items-center justify-center">
 				<div
-					className="h-30 w-30 flex items-center justify-center bg-neutral-800 font-heading text-5xl text-center text-primary relative"
+					className="h-20 w-20 flex items-center justify-center bg-neutral-800 font-heading text-5xl text-center text-primary relative"
 					style={{
 						clipPath: "polygon(20% 0%, 100% 0%, 100% 80%, 80% 100%, 0% 100%, 0% 20%)",
 					}}
@@ -113,7 +129,7 @@ const BotPreview = ({
 		<img
 			src={botDetails.photo ? `/${botDetails.photo}` : undefined}
 			className={cn(
-				"h-40 text-rrgreen",
+				"max-w-35 max-h-25 object-contain text-rrgreen",
 				bot?.isDead && "greyscale-manual",
 				teamColor === "blue" && "transform -scale-x-100",
 				bot?.isSubbed && "text-primary",
@@ -149,14 +165,19 @@ const TeamInfo = ({
 	const botIndexes = teamColor === "yellow" ? [0, 1, 2, 3, 4] : [4, 3, 2, 1, 0];
 
 	return (
-		<div className="flex flex-col w-full">
-			<BotInfo bot={activeBot} participants={participants} teamColor={teamColor} />
+		<div className="flex flex-col w-full h-full justify-end">
+			<div className="flex-1 flex flex-col justify-end min-h-[500px]">
+				<BotInfo bot={activeBot} participants={participants} teamColor={teamColor} />
+			</div>
 			<div
-				className={cn("text-4xl font-rubik text-white mt-15", teamColor === "yellow" ? "text-rryellow" : "text-rrblue")}
+				className={cn(
+					"text-4xl font-rubik text-white mt-16 min-h-[60px] flex items-center justify-center",
+					teamColor === "yellow" ? "text-rryellow" : "text-rrblue",
+				)}
 			>
 				{teamName}
 			</div>
-			<div className="mx-10 flex flex-row justify-between items-center">
+			<div className="mx-10 flex flex-row justify-between items-center mt-4 min-h-[100px]">
 				{botIndexes.map((index) => (
 					<BotPreview
 						key={`${teamName}-${index}`}
@@ -184,12 +205,12 @@ function RouteComponent() {
 	).length;
 
 	return (
-		<div className="flex flex-row h-full gap-x-40 relative">
+		<div className="grid grid-cols-2 h-full gap-x-40 relative pb-10 pt-25">
 			<div className="absolute left-1/2 transform -translate-x-1/2 text-center text-primary font-heading mt-4 ">
 				<h3 className="text-[40px]">FIGHT {numberOfBotsWithIds - 1}</h3>
 				<h3 className="text-[50px] mt-100">VS</h3>
 			</div>
-			<div className="flex flex-1 items-end justify-center text-center">
+			<div className="flex flex-col justify-end text-center h-full">
 				<TeamInfo
 					teamName={schedule.team1Name}
 					bots={schedule.team1bots}
@@ -197,7 +218,7 @@ function RouteComponent() {
 					teamColor="yellow"
 				/>
 			</div>
-			<div className="flex flex-1 items-end justify-center text-center">
+			<div className="flex flex-col justify-end text-center h-full">
 				<TeamInfo
 					teamName={schedule.team2Name}
 					bots={schedule.team2bots}
