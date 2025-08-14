3145b1374f5f3564cc8a2ff916e93791f0eadb5f
Reeve D'Cunha <dcunhareeve@gmail.com>
2025-08-15 01:47:00 +1200

    responsive commentator guide

--- src/routes/admin_/mobile/$id.$matchId/info.tsx
+++ src/routes/admin_/mobile/$id.$matchId/info.tsx
@@ -7,6 +7,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Carousel, type CarouselApi, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 import { Separator as SeparatorComponent } from "@/components/ui/separator";
 import type { Event, Match, Participant } from "@/db";
+import { useIsTablet } from "@/hooks/use-mobile";
 import { cn, createBorderEffect } from "@/lib/utils";
 import { dbMiddleware } from "@/middleware";
 import { getSchedule } from "@/routes/admin/schedule/$id";
@@ -53,6 +54,8 @@ const BotPreview = ({
 	stats: Event["qualifyingResults"];
 	toggleSelectedBot: () => void;
 }) => {
+	const isTablet = useIsTablet();
+
 	const [api, setApi] = useState<CarouselApi>();
 	const qualifyingPosition = rankings.find((r) => r.id === bot.id)?.position;
 	const botStats = stats[bot.id];
@@ -69,13 +72,13 @@ const BotPreview = ({
 
 	return (
 		<div className="flex flex-col gap-2 items-center">
-			<Carousel setApi={setApi} opts={{ loop: true }} className="flex-1 w-full h-full">
+			<Carousel setApi={setApi} opts={{ loop: true, watchDrag: !isTablet }} className="flex-1 w-full h-full">
 				<CarouselContent className="h-full" wrapperClassName="h-full">
 					<CarouselItem className="flex flex-col gap-2 items-center">
 						<img
 							src={`/${bot.photo}`}
 							alt={bot.name}
-							className="h-20 text-primary"
+							className="h-20 text-primary mt-4"
 							height={80}
 							style={
 								{
@@ -84,7 +87,7 @@ const BotPreview = ({
 							}
 						/>
 
-						<div className="text-primary font-heading uppercase text-xl flex items-center gap-4">
+						<div className="text-primary font-heading uppercase text-xl md:text-3xl flex items-center gap-4">
 							{qualifyingPosition && (
 								<span className="bg-yellow-400 w-[2ch] h-[2ch] aspect-square rounded text-black font-heading uppercase text-md flex items-center justify-center">
 									{qualifyingPosition}
@@ -94,14 +97,14 @@ const BotPreview = ({
 						</div>
 						{botStats && (
 							<div className="mb-0 -mt-2">
-								<span className="font-bold flex gap-2 text-lg">
+								<span className="font-bold flex gap-2 text-lg md:text-3xl">
 									<span className="text-green-500 rounded-md w-[3ch] text-right  ">{`${botStats.wins}W`}</span>
 									<span className="w-[2ch] text-center">|</span>
 									<span className="text-red-500 w-[3ch] text-left ">{`${botStats.losses}L`}</span>
 								</span>
 							</div>
 						)}
-						<div className="text-center uppercase text-lg font-bold -mt-1">{bot.weapon}</div>
+						<div className="text-center uppercase text-lg md:text-3xl font-bold -mt-1">{bot.weapon}</div>
 					</CarouselItem>
 					<CarouselItem></CarouselItem>
 				</CarouselContent>
@@ -122,9 +125,15 @@ const LabelledDetails = ({
 	isUppercase?: boolean;
 }) => {
 	return (
-		<div className={cn("flex w-full gap-2", isVertical && "flex-col")}>
-			<div className={cn("text-lg font-bold text-yellow-400 uppercase")}>{label}</div>
-			<div className={cn("text-lg font-bold flex-1", !isVertical && "text-right", isUppercase && "uppercase")}>
+		<div className={cn("flex w-full gap-2 md", isVertical && "flex-col")}>
+			<div className={cn("text-lg md:text-3xl lg:text-3xl font-bold text-yellow-400 uppercase")}>{label}</div>
+			<div
+				className={cn(
+					"text-lg md:text-3xl lg:text-3xl font-bold flex-1",
+					!isVertical && "text-right",
+					isUppercase && "uppercase",
+				)}
+			>
 				{value ?? "-"}
 			</div>
 		</div>
@@ -133,7 +142,7 @@ const LabelledDetails = ({
 
 const Slide1 = ({ bot }: { bot: Participant }) => {
 	return (
-		<div className="flex flex-col gap-2">
+		<div className="flex flex-col gap-2 md:gap-6">
 			<LabelledDetails label="Driver" value={bot.builders} />
 			<LabelledDetails label="Town" value={bot.town} />
 			<LabelledDetails label="Occupation" value={bot.occupation} />
@@ -146,7 +155,7 @@ const Slide1 = ({ bot }: { bot: Participant }) => {
 
 const Slide2 = ({ bot }: { bot: Participant }) => {
 	return (
-		<div className="flex flex-col gap-2">
+		<div className="flex flex-col gap-2 md:gap-6">
 			<LabelledDetails label="Other Notes" value={bot.otherNotes} isVertical isUppercase={false} />
 		</div>
 	);
@@ -154,7 +163,7 @@ const Slide2 = ({ bot }: { bot: Participant }) => {
 
 const Slide3 = ({ bot, match }: { bot: Participant; match: Match }) => {
 	return (
-		<div className="flex flex-col gap-2">
+		<div className="flex flex-col gap-2 md:gap-6">
 			<LabelledDetails
 				label="Match Intro"
 				value={match.participants.find((p) => p.id === bot.id)?.introText ?? ""}
@@ -165,10 +174,19 @@ const Slide3 = ({ bot, match }: { bot: Participant; match: Match }) => {
 	);
 };
 
-const BotInfo = ({ bot, match }: { bot: Participant; match: Match }) => {
+const BotInfo = ({
+	bot,
+	otherBot,
+	match,
+}: {
+	bot: Participant;
+	otherBot: Participant | undefined | null;
+	match: Match;
+}) => {
 	const [api, setApi] = useState<CarouselApi>();
 	const [current, setCurrent] = useState(0);
 	const [count, setCount] = useState(0);
+	const isTablet = useIsTablet();
 
 	useEffect(() => {
 		if (!api) {
@@ -184,7 +202,7 @@ const BotInfo = ({ bot, match }: { bot: Participant; match: Match }) => {
 	}, [api]);
 
 	return (
-		<Carousel setApi={setApi} className="flex-1 w-full h-full">
+		<Carousel opts={{ loop: true }} setApi={setApi} className="flex-1 w-full h-full flex flex-col gap-2">
 			<div className="-mt-4 flex items-center justify-center py-2 gap-2">
 				{Array.from({ length: count }).map((_, index) => (
 					<div
@@ -196,25 +214,52 @@ const BotInfo = ({ bot, match }: { bot: Participant; match: Match }) => {
 			</div>
 			<CarouselContent className="h-full" wrapperClassName="h-full">
 				<CarouselItem className="h-full">
-					<Card className="flex-1 h-full">
-						<CardContent>
-							<Slide1 bot={bot} />
-						</CardContent>
-					</Card>
+					<div className="flex gap-10 h-full">
+						<Card className="flex-1 h-full">
+							<CardContent>
+								<Slide1 bot={bot} />
+							</CardContent>
+						</Card>
+						{isTablet && otherBot && (
+							<Card className="flex-1 h-full">
+								<CardContent>
+									<Slide1 bot={otherBot} />
+								</CardContent>
+							</Card>
+						)}
+					</div>
 				</CarouselItem>
 				<CarouselItem className="h-full">
-					<Card className="flex-1 h-full">
-						<CardContent>
-							<Slide2 bot={bot} />
-						</CardContent>
-					</Card>
+					<div className="flex gap-10 h-full">
+						<Card className="flex-1 h-full">
+							<CardContent>
+								<Slide2 bot={bot} />
+							</CardContent>
+						</Card>
+						{isTablet && otherBot && (
+							<Card className="flex-1 h-full">
+								<CardContent>
+									<Slide2 bot={otherBot} />
+								</CardContent>
+							</Card>
+						)}
+					</div>
 				</CarouselItem>
 				<CarouselItem className="h-full">
-					<Card className="flex-1 h-full">
-						<CardContent>
-							<Slide3 bot={bot} match={match} />
-						</CardContent>
-					</Card>
+					<div className="flex gap-10 h-full">
+						<Card className="flex-1 h-full">
+							<CardContent>
+								<Slide3 bot={bot} match={match} />
+							</CardContent>
+						</Card>
+						{isTablet && otherBot && (
+							<Card className="flex-1 h-full">
+								<CardContent>
+									<Slide3 bot={otherBot} match={match} />
+								</CardContent>
+							</Card>
+						)}
+					</div>
 				</CarouselItem>
 			</CarouselContent>
 		</Carousel>
@@ -234,27 +279,31 @@ const BotSelector = ({
 	selectedBot: Participant | null;
 	setSelectedBot: (bot: Participant | null) => void;
 }) => {
+	const isTablet = useIsTablet();
+
 	return (
 		<Card className="gap-2 p-2">
 			<CardHeader className="text-center">
-				<CardTitle className="text-xl font-bold text-primary font-heading uppercase">{matchName}</CardTitle>
+				<CardTitle className="text-xl md:text-3xl font-bold text-primary font-heading uppercase">{matchName}</CardTitle>
 			</CardHeader>
-			<CardContent className="flex gap-2 p-0">
-				<Button
-					className="flex-1 text-l py-2 uppercase font-bold"
-					variant={selectedBot === bot1 ? "default" : "secondary"}
-					onClick={() => setSelectedBot(bot1 ?? null)}
-				>
-					{bot1?.name ?? "TBD"}
-				</Button>
-				<Button
-					className="flex-1 text-l py-2 uppercase font-bold"
-					variant={selectedBot === bot2 ? "default" : "secondary"}
-					onClick={() => setSelectedBot(bot2 ?? null)}
-				>
-					{bot2?.name ?? "TBD"}
-				</Button>
-			</CardContent>
+			{!isTablet && (
+				<CardContent className="flex gap-2 p-0 ">
+					<Button
+						className="flex-1 text-l py-2 uppercase font-bold md:text-2xl md:py-6"
+						variant={selectedBot === bot1 ? "default" : "secondary"}
+						onClick={() => setSelectedBot(bot1 ?? null)}
+					>
+						{bot1?.name ?? "TBD"}
+					</Button>
+					<Button
+						className="flex-1 text-l py-2 uppercase font-bold md:text-2xl md:py-6"
+						variant={selectedBot === bot2 ? "default" : "secondary"}
+						onClick={() => setSelectedBot(bot2 ?? null)}
+					>
+						{bot2?.name ?? "TBD"}
+					</Button>
+				</CardContent>
+			)}
 		</Card>
 	);
 };
@@ -264,6 +313,7 @@ function RouteComponent() {
 	const { schedule, participants, rankings, stats } = Route.useLoaderData();
 	const match = schedule.matches.find((match) => match.id === matchId);
 	const [selectedBot, setSelectedBot] = useState<Participant | null>(null);
+	const isTablet = useIsTablet();
 
 	// Set the selected bot after data is loaded to prevent flickering
 	useEffect(() => {
@@ -293,6 +343,8 @@ function RouteComponent() {
 		}
 	};
 
+	const otherBot = selectedBot && (selectedBot.id === bot1?.id ? bot2 : bot1);
+
 	return (
 		<div className="flex flex-col gap-8 p-4 h-full">
 			<BotSelector
@@ -303,15 +355,26 @@ function RouteComponent() {
 				setSelectedBot={setSelectedBot}
 			/>
 			{selectedBot && (
-				<BotPreview
-					key={selectedBot.id}
-					bot={selectedBot}
-					toggleSelectedBot={toggleSelectedBot}
-					rankings={rankings}
-					stats={stats}
-				/>
+				<div className={cn("grid grid-cols-1 gap-10", isTablet && "grid-cols-2")}>
+					<BotPreview
+						key={selectedBot.id}
+						bot={selectedBot}
+						toggleSelectedBot={toggleSelectedBot}
+						rankings={rankings}
+						stats={stats}
+					/>
+					{isTablet && otherBot && (
+						<BotPreview
+							key={otherBot.id}
+							bot={otherBot}
+							toggleSelectedBot={toggleSelectedBot}
+							rankings={rankings}
+							stats={stats}
+						/>
+					)}
+				</div>
 			)}
-			{selectedBot && <BotInfo bot={selectedBot} match={match} />}
+			{selectedBot && <BotInfo bot={selectedBot} otherBot={otherBot} match={match} />}
 		</div>
 	);
 }
