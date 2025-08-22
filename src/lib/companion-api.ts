d0c5337ace29040357cb8ff3485b758bd4a15a14
Reeve D'Cunha <dcunhareeve@gmail.com>
2025-08-23 04:09:54 +1200

    companion helper method

--- src/lib/companion-api.ts
+++ src/lib/companion-api.ts
@@ -1,15 +1,19 @@
 class CompanionApi {
 	private ENDPOINT = "http://192.168.50.244:8000/api";
 
-	async RunMatchEndSequence() {
+	async callButton(page: number, row: number, column: number) {
 		try {
-			await fetch(`${this.ENDPOINT}/location/11/3/5/press`, {
+			await fetch(`${this.ENDPOINT}/location/${page}/${row}/${column}/press`, {
 				method: "POST",
 			});
 		} catch (error) {
 			console.error(error);
 			return null;
 		}
+	}
+
+	async RunMatchEndSequence() {
+		await this.callButton(11, 3, 5);
 	}
 }
 
